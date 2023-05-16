ab1d3cc89353284316ae7f99f458778a8cf68ad1
EC2 Default User <ec2-user@ip-172-31-45-244.ap-northeast-1.compute.internal>
2023-05-16 14:59:12 +0000

    Start

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+HTTPメソッド 'URL' => 'コントローラ#アクション'
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top'
+end
